e0d4f75d9ad3cc6421f5091ab0cf6286915d9874
Graziano Bolla <grazianobolla12@gmail.com>
2021-08-09 20:58:23 -0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/grazianobolla/wildermyth-translator
+
+go 1.16
+
+require (
+	github.com/robertkrimen/otto v0.0.0-20210614181706-373ff5438452
+	gopkg.in/sourcemap.v1 v1.0.5 // indirect
+)
---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strings"
+
+	"github.com/grazianobolla/wildermyth-translator/translator"
+)
+
+var code_regex, _ = regexp.Compile(`\<.*?\>`)
+var line_regex, _ = regexp.Compile(`=.*`)
+
+func main() {
+	var file_path_flag = flag.String("file", "", "File path")
+	flag.Parse()
+
+	process_file(*file_path_flag)
+}
+
+func process_file(file_path string) {
+	file, err := os.Open(file_path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if (len(line) > 0 && line[0] == '#') || len(line) <= 0 {
+			continue
+		}
+
+		str := parse_line(line_regex.FindString(line)[1:])
+
+		res := line_regex.ReplaceAllString(line, "="+str)
+		fmt.Println(res)
+	}
+}
+
+func parse_line(line string) string {
+	prev_str := code_regex.FindAllString(line, -1)
+	line = code_regex.ReplaceAllString(line, "(he)")
+	res := translate_text(line)
+
+	//put back code stuff
+	index := 0
+	for {
+		if index >= len(prev_str) {
+			break
+		}
+
+		res = strings.Replace(res, "(él)", prev_str[index], 1)
+		index++
+	}
+
+	return res
+}
+
+func translate_text(text string) string {
+	str, err := translator.Translate(text, "en", "es")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return str
+}
